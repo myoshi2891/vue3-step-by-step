b96a6d29dab410f747813f9b4d46a2a1067a36ac
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-18 23:38:10 +0900

    Handle a Form Submission

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -7,6 +7,13 @@ export default {
     <section class="space-y-6">
         <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+        <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black">
+                <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
+                <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+        </form>
     </section>
     `,
 	data() {
@@ -16,6 +23,8 @@ export default {
 				{ name: "Read chapter 4", complete: false, id: 2 },
 				{ name: "Turn in homework", complete: false, id: 3 },
 			],
+
+			newAssignment: "",
 		};
 	},
 
@@ -29,6 +38,18 @@ export default {
 					(assignment) => assignment.complete
 				),
 			};
+		},
+	},
+
+	methods: {
+		add() {
+			this.assignments.push({
+				name: this.newAssignment,
+				completed: false,
+				id: this.assignments.length + 1,
+			});
+
+			this.newAssignment = "";
 		},
 	},
 };
