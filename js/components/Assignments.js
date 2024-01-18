afa9244980e0fb4723e46c2f6a95953c0c064060
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-18 23:51:53 +0900

    Parent-Child State Communication

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,19 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-	components: { AssignmentList },
+	components: { AssignmentList, AssignmentCreate },
 
 	template: /* html */ `
     <section class="space-y-6">
         <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-        <form @submit.prevent="add">
-            <div class="border border-gray-600 text-black">
-                <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
-                <button type="submit" class="bg-white p-2 border-l">Add</button>
-            </div>
-        </form>
+        <assignment-create @add="add"></assignment-create>
     </section>
     `,
 	data() {
@@ -23,8 +19,6 @@ export default {
 				{ name: "Read chapter 4", complete: false, id: 2 },
 				{ name: "Turn in homework", complete: false, id: 3 },
 			],
-
-			newAssignment: "",
 		};
 	},
 
@@ -42,14 +36,12 @@ export default {
 	},
 
 	methods: {
-		add() {
+		add(name) {
 			this.assignments.push({
-				name: this.newAssignment,
+				name: name,
 				completed: false,
 				id: this.assignments.length + 1,
 			});
-
-			this.newAssignment = "";
 		},
 	},
 };
